d8a082fac0b575f15856131e246d1ac579dba6c2
91934 <91934@LAPTOP-7N07FBDU>
2021-06-11 12:55:49 +0530

    First comitt...

---
+++ EmployeeManagement/src/Employee.java
@@ -0,0 +1,35 @@
+
+public class Employee {
+	private int id;
+	private String name;
+	private long salary;
+
+	public static void main() {
+    
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
